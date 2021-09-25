a17594ca544481bf926343a5d1ffc619c5fcff91
Przemek Mela <develop4@fa1.pl>
2021-09-25 21:44:25 +0200

    Instaled PrimeNg framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './components/search-criteria/search-criteria.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import {MultiSelectModule} from 'primeng/multiselect';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    MultiSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
